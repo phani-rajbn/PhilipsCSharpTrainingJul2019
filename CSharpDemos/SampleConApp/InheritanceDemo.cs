0f5d10bffbe40264fea834c1d3ae69ed7b558a62
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2019-07-09 14:48:10 +0530

    Add files via upload

---
+++ CSharpDemos/SampleConApp/InheritanceDemo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class BaseClass
+    {
+        public void BaseFunc() => Console.WriteLine("Base Func implemented");
+    }
+    //: is used to extend a Class into another. Inheritance is used for reusability. In this case, a Class with X number of methods could be extended to another class which will add Y no of methods, where the new class will have (X+Y) methods in it. U dont have to delegate or implement the old methods. 
+    class DerivedClass :  BaseClass
+    {
+        public void DerivedFunc() => Console.WriteLine("Derived Func Implemented");
+    }
+    class InheritanceDemo
+    {
+        static void Main(string[] args)
+        {
+            //DerivedClass cls = new DerivedClass();
+            //cls.BaseFunc();//base class func
+            //cls.DerivedFunc();//derived class func...
+
+            BaseClass baseCls = new BaseClass();
+            baseCls.BaseFunc();
+
+            //baseCls = new DerivedClass();//Luskov's substitution principle, makes sense in Overriding...
+            //Upcasting....
+            //Even if the object is instantiated to the derived class, U cannot access the members of the derived. To do so, U should typecast it to the derived and then access its methods...
+
+            //((DerivedClass)baseCls).DerivedFunc();
+            if (baseCls is DerivedClass)//More safe.....
+            {
+                DerivedClass variable = baseCls as DerivedClass;//is and as operators work only on reference types. as it is a safe way of Unboxing or type conversions for reference types..
+                variable.DerivedFunc();
+            }
+        }
+    }
+    /*Points to remember:
+     * C# supports Single Inheritance. It means that the derived class can have only one base class at any level. However U could extend to multiple layers, so that it supports Multi-Layered Inheritance. 
+     * There is no access specifier while inheriting into another class. All the members of the base class will retain the accessibility in the derived classes:
+     * private methods are inaccessible to the derived classes.
+     * protected, internal and public methods retain their accessibility in the derived classes
+     * internal means accessibility within the Project(Assembly).
+     * For the constructors, the base class constructor will be called before the derived class constructor. 
+     */
+}
---
+++ CSharpDemos/SampleConApp/MethodOverriding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Its a feature where the derived class will modify the functionality without breaking the signature of the base method. The base class should mark those methods that could be modified as virtual and the derived class will override(reimplement) thro methods using override keyword, virtual is more like a permission to override and override is like a notification that it intends to modify
+namespace SampleConApp
+{
+    class TestClass
+    {
+        public virtual void TestFunc() => Console.WriteLine("Test Func from base");
+
+        public void NormalFunc() => Console.WriteLine("Normal Func");
+    }
+
+    class NewTestClass : TestClass
+    {
+        public override void TestFunc() => Console.WriteLine("Test Func from derived");
+    }
+    class MethodOverriding
+    {
+        static void Main(string[] args)
+        {
+            TestClass cls = new TestClass();
+            cls.TestFunc();
+
+            cls = new NewTestClass();
+            cls.TestFunc();//Runtime Polymorphism achieved thro method overriding...
+        }
+    }
+}
